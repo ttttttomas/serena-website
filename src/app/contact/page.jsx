82dbbbc31a7aed64f7783d9138780e5dc09f2159
ttttttomas <totobarajas124@gmail.com>
2024-11-26 17:22:45 -0300

    add aereos page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -10,13 +10,17 @@ function Contacto() {
         <h1 className="mt-5 mb-3 font-bold text-orangeMedium text-3xl text-center">
             Contáctanos que nos gusta pedir proyectos gratis!
         </h1>
-        <div className="container mx-auto">
-            <form className="bg-creamBg">
-                <input type="text" />
-                <input type="text" />
-                <input type="email" />
-                <input type="text" />
-                <textarea name="" id=""></textarea>
+        <div className="flex justify-center items-center">
+            <form className="bg-creamBg mx-auto flex gap-10 flex-col">
+                <div>
+                <input className="bg-white" type="text" />
+                <input className="bg-white" type="text" />
+                </div>
+                <div>
+                <input className="bg-white" type="email" />
+                <input className="bg-white" type="text" />
+                </div>
+                <textarea className="w-full" name="" id=""></textarea>
                 <button className="bg-orangeMedium text-white font-medium" type="submit">Enviar</button>
             </form>
         </div>
