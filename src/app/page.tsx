cbdfebd00c88dc97780b5fb62a59242340ed8f0a
ttttttomas <totobarajas124@gmail.com>
2024-12-09 15:45:47 -0300

    fixed responsive home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,24 +81,24 @@ export default function Home() {
     <section id="opinions" className='flex flex-col justify-center'>
       <h4 className='text-3xl text-center mb-20'>ALGUNAS DE NUESTRAS RESEÑAS</h4>
         <ul className='flex justify-center text-center flex-wrap'>
-          <li className='relative p-20 mx-5'>
-            <div className='absolute -top-7 left-[150px]'>
+          <li className='relative md:p-20 py-20 mx-5'>
+            <div className='absolute -top-7 md:left-[150px] left-[80px]'>
             <OpinionExample />
             </div>
             <div className='flex justify-center items-center z-50 bg-opinionBg h-52 w-80'>
               <p className='z-40 text-center px-10'>"bla bla bla bla blabla bla bla bla blabla bla bla bla blabla bla bla bla blabla bla bla bla blabla bla bla bla bla"</p>
             </div>
           </li>
-          <li className='relative p-20'>
-            <div className='absolute -top-7 left-[150px]'>
+          <li className='relative md:p-20 py-20 mx-5'>
+            <div className='absolute -top-7 md:left-[150px] left-[80px]'>
             <OpinionExample />
             </div>
             <div className='flex justify-center items-center z-50 bg-opinionBg h-52 w-80'>
               <p className='z-40 text-center px-10'>"bla bla bla bla blabla bla bla bla blabla bla bla bla blabla bla bla bla blabla bla bla bla blabla bla bla bla bla"</p>
             </div>
           </li>
-          <li className='relative p-20'>
-            <div className='absolute -top-7 left-[150px]'>
+          <li className='relative md:p-20 py-20 mx-5'>
+            <div className='absolute -top-7 md:left-[150px] left-[80px]'>
             <OpinionExample />
             </div>
             <div className='flex justify-center items-center z-50 bg-opinionBg h-52 w-80'>
